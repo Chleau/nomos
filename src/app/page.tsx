5261b48f6484dd86866da4bb0d8d2184ed5d8130
Chloé <michelchloe04@gmail.com>
2025-09-24 11:37:04 +0200

    signalement, création

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import ListeHabitants from '@/components/ListeHabitants'
+import SignalementForm from '@/components/signalements/SignalementForm'
 
 export default function Home() {
   return (
     <main className="min-h-screen p-4">
       <ListeHabitants />
-      <h1>Bonjour</h1>
+      <SignalementForm />
     </main>
   )
 }
